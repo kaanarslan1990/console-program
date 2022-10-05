654bad0d8fa508fe81d5fcbd396cc7c27ee4fe89
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-10-05 15:01:48 +0300

    my first project c#

---
+++ practise/console-program/console-programlama/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace console_programlama
+{
+    internal class Program
+
+    {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+        Console.WriteLine("Fill your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Fill your surname:");
+        string surName = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name + " " + surName);
+
+
+    }
+    }
+}
