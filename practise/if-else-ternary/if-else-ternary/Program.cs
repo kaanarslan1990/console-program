2374b4e888a30a9398482ec7505badd04420f662
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-10-11 16:46:47 +0300

    if-else-ternary

---
+++ practise/if-else-ternary/if-else-ternary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_else_ternary
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Good morning Sir!");
+            }
+
+            else if (time >=13 &&time < 18)
+            {
+                Console.WriteLine("Good afternoon Sir!");
+            }
+            else
+            {
+                Console.WriteLine("Have good night Sir!");
+            }
+
+            string sonuc = time <= 18 ? "Good day!" : "Good night";
+            sonuc = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi günler" : "İyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
