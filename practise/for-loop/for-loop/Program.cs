d5cc3aa3c932dec48605924806ac9cc0541bbc92
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-11-19 11:25:59 +0300

    for loop and iterators

---
+++ practise/for-loop/for-loop/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace for_loop
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // // Print odd number to console lower than input number
+            // Console.WriteLine("Please enter a number:");
+            //
+            // int counter = int.Parse(Console.ReadLine());
+            //
+            // for (int i = 0; i <= counter; i++)
+            // {
+            //     if(i%2 ==1 )
+            //         Console.WriteLine(i);
+
+                // komutlar
+            // }
+
+            // odd and even number total between 1 and 1000
+            // int oddTotal = 0;
+            // int evenTotal = 0;
+            // for (int i = 0; i <= 1000; i++)
+            // {
+            //     if (i % 2 == 1)
+            //         oddTotal += i;
+            //     else
+            //         evenTotal += i;
+            //
+            // }
+            //
+            // Console.WriteLine("Odd total= " + oddTotal);
+            // Console.WriteLine("Even total= " + evenTotal);
+
+            for (int i = 1; i < 10; i++)
+            {
+
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
