977fccf9eca3776bb5eb3a57595106e186096dce
kaanarslan1990 <oguzkaanarslan@outlook.com>
2023-01-28 10:31:08 +0300

    methods

--- practise/array_class_methods/array_class_methods/Program.cs
+++ practise/array_class_methods/array_class_methods/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace array_class_methods
 {
@@ -7,60 +8,74 @@ namespace array_class_methods
         public static void Main(string[] args)
         {
 
-            // Sort
+          
 
-            int[] numArray = { 23, 12, 4, 86, 72, 3, 11, 17 };
+            // int maxNum = candles.Max();
+            // return candles.Select(c => c.Equals(maxNum)).Where(x => x).Count();
+
+            // Sort
+            int[] numArray = { 23, 12, 4, 86, 72, 3, 11,86, 17 };
+           int count = 0;
+           int maxNum = numArray.Max();
 
             Console.WriteLine("******* Unordered List******");
             foreach (var num in numArray)
             {
-                Console.WriteLine(num);
-            }
-
-            Console.WriteLine("*******Ordered List******");
-            Array.Sort(numArray);
-            foreach (var num in numArray)
-            {
-                Console.WriteLine(num);
-            }
-
-
-            // Clear
-            Console.WriteLine("*******Array Clear******");
-
-            Array.Clear(numArray,2,2);
-            foreach (var num in numArray)
-            {
-                Console.WriteLine(num);
-            }
-
-            // Reverse
-            Console.WriteLine("*******Array Reverse******");
-
-            Array.Reverse(numArray);
-            foreach (var num in numArray)
-            {
-                Console.WriteLine(num);
-            }
-
-            // IndexOf
-            Console.WriteLine("*******IndexOf******");
-
-
-
-            Console.WriteLine(Array.IndexOf(numArray, 23));
-
-
-            // Resize
-            Console.WriteLine("*******Resize******");
-
-            Array.Resize<int>(ref numArray, 9);
-            numArray[8] = 99;
-            foreach (var num in numArray)
-            {
-                Console.WriteLine(num);
+                if (num == maxNum)
+                {
+                    count++;
+                }
 
             }
+
+            return;
+
+            Console.WriteLine(count);
+
+            // Console.WriteLine("*******Ordered List******");
+            // Array.Sort(numArray);
+            // foreach (var num in numArray)
+            // {
+            //     Console.WriteLine(num);
+            // }
+            //
+            //
+            // // Clear
+            // Console.WriteLine("*******Array Clear******");
+            //
+            // Array.Clear(numArray,2,2);
+            // foreach (var num in numArray)
+            // {
+            //     Console.WriteLine(num);
+            // }
+            //
+            // // Reverse
+            // Console.WriteLine("*******Array Reverse******");
+            //
+            // Array.Reverse(numArray);
+            // foreach (var num in numArray)
+            // {
+            //     Console.WriteLine(num);
+            // }
+            //
+            // // IndexOf
+            // Console.WriteLine("*******IndexOf******");
+            //
+            //
+            //
+            // Console.WriteLine(Array.IndexOf(numArray, 23));
+            //
+            //
+            // // Resize
+            // Console.WriteLine("*******Resize******");
+            //
+            // Array.Resize<int>(ref numArray, 9);
+            // numArray[8] = 99;
+            // foreach (var num in numArray)
+            // {
+            //     Console.WriteLine(num);
+            //
+            // }
 
 
 
---
+++ practise/methods/methods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace methods
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+             // erisim_belirteci geri_donustipi metot_adi(parametreListesi/argüman)
+             // {
+             //     //komutlar;
+             //     //return
+             // }
+
+             int a = 2;
+             int b = 3;
+
+             Console.WriteLine(a+b);
+
+             int sonuc = Topla(a,b);
+             Console.WriteLine(sonuc);
+
+             Metotlar ornek = new Metotlar();
+             ornek.EkranaYazdır(Convert.ToString("sonuc:"+sonuc));
+
+
+
+             int sonuc2 = ornek.IncAndSum(ref a, ref b);
+             ornek.EkranaYazdır(Convert.ToString(sonuc2));
+             ornek.EkranaYazdır(Convert.ToString(a+b));
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdır(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int IncAndSum(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+
+    }
+}
